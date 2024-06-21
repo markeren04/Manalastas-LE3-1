d929bd76ed0759854dd8505c163fe6f0057276d0
markeren04 <meimanalastas@mymail.mapua.edu.ph>
2024-06-21 18:02:11 +0800

    Add files via upload

---
+++ ManalastasBlogDB/Stored Procedures/spUsers_Register.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_Register]
+	@userName nvarchar (16),
+	@firstName nvarchar (50),
+	@lastName nvarchar (50),
+	@password nvarchar (16)
+
+AS
+begin
+	set nocount on
+
+	INSERT INTO dbo.Users
+	(UserName, FirstName, LastName, Password)
+	VALUES (@userName, @FirstName, @LastName, @password)
+
+end
